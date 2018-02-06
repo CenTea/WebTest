be5f618ad5a3dc2ec9e27661a9ba94284cc2d109
CenTea <34390309+CenTea@users.noreply.github.com>
2018-02-05 16:45:26 -0800

    Update webtest.js

--- webtest.js
+++ webtest.js
@@ -1,7 +1,7 @@
-  function addtwo()
+  function addtwo(id1, id2, final)
   {
-   var first_number = parseInt(document.getElementById("number1").value);
-   var second_number = parseInt(document.getElementById("number2").value);
+   var first_number = parseInt(document.getElementById(id1).value);
+   var second_number = parseInt(document.getElementById(id2).value);
    var result = first_number + second_number;
-   document.getElementById("box").value = result;
+   document.getElementById(final).value = result;
   }
