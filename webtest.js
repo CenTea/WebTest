7e1040c9981aada14f67ea6cf8ebe990106e9008
CenTea <34390309+CenTea@users.noreply.github.com>
2018-02-05 16:49:58 -0800

    Delete webtest.js

--- webtest.js
+++
@@ -1,7 +0,0 @@
-  function addtwo(id1, id2, final)
-  {
-   var first_number = parseInt(document.getElementById(id1).value);
-   var second_number = parseInt(document.getElementById(id2).value);
-   var result = first_number + second_number;
-   document.getElementById(final).value = result;
-  }
