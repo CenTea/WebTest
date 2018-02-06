27ec9dc7ca01afcbc4195137a40aa7dc030f4262
CenTea <34390309+CenTea@users.noreply.github.com>
2018-02-05 16:40:46 -0800

    Create webtest.js

---
+++ webtest.js
@@ -0,0 +1,9 @@
+<script type="text/javascript">
+  function addtwo()
+  {
+   var first_number = parseInt(document.getElementById("number1").value);
+   var second_number = parseInt(document.getElementById("number2").value);
+   var result = first_number + second_number;
+   document.getElementById("box").value = result;
+  }
+</script>
